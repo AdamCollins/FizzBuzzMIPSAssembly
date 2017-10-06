bffeabf2e3d282228d1e3774614062895250dba8
Adam Collins <adamcollinssecure@gmail.com>
2017-10-06 11:52:36 -0700

    First Commit

--- FizzBuzz.asm
+++ FizzBuzz.asm
@@ -2,26 +2,26 @@
 .data
 FIZZ:	.asciiz "FIZZ"
 BUZZ:	.asciiz	"BUZZ"
+
 .text
 li $t3 3	#t3 = 3
 li $t5 5	#t5 = 5 	
 
 li $t0 15	#n = t0
 
-
 div $t0 $t3	#lo = t0%3
 mfhi $s0	#s0 = t0%3
-beq $s0 $0 printFizz
+beq $s0 $0 printFizz	#print FIZZ if t0%3==0
 	printFizzNext:
 
-div $t0 $t5	#lo = t0%5
+div $t0 $t5	#lo = t0%5	#print BUZZ if t0%5==0
 mfhi $s0	#s0 = t0%5
 beq $s0 $0 printBuzz
 	printBuzzNext:
 
-#Space to expand to other multiples
-j end
+#Space to expand with other multiples
 
+j end		#end Program
 
 printFizz:
 	li $v0 4
